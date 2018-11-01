ac456a7790ca22c603c5037673e0193b7a230944
kwartler <ehk116@gmail.com>
2018-11-01 16:17:44 -0400

    lesson2 wip

---
+++ scripts/2_LendingClub_B.R
@@ -0,0 +1,7 @@
+#' Author: Ted Kwartler
+#' Date: 11-1-2018
+#' Purpose: Load Model and score new notes
+#'
+
+# WD
+setwd("C:/Users/Edward/Desktop/odsc blogs/presentation/ODSC_west_2018/workshop_data/LC")
